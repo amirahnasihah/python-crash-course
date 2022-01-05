3bf58138bbd35517b5518c13270ce5b803726601
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-01-05 21:34:18 +0800

    Create 5-1. Conditional Tests.py

---
+++ 5. if statements/5. exercises/5-1. Conditional Tests.py
@@ -0,0 +1,17 @@
+''' Write a series of conditional tests. Print a statement
+describing each test and your prediction for the results of each test '''
+
+cars = ['toyota', 'mercedes', 'subaru', 'honda']
+
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+
+print("\nIs car != 'honda'? I predict True.")
+print(car != 'honda')
+
+cars != 'honda'
+print("\nIs 'honda' is not in cars?")
