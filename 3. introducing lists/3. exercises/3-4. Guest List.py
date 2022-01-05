a9abc2957e3beb5664d5c605ca5cc25ef4ecbd51
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-01-05 21:29:17 +0800

    Create 3-4. Guest List.py

---
+++ 3. introducing lists/3. exercises/3-4. Guest List.py
@@ -0,0 +1,27 @@
+# c3; pg 80 try it yourself
+
+
+#3-4. Guest List: 
+'''If you could invite anyone, living or deceased, to dinner, who
+would you invite? Make a list that includes at least three people you’d like to
+invite to dinner. Then use your list to print a message to each person, inviting
+them to dinner. '''
+guests = ['seha', 'eunji', 'miru']
+
+print(f"You are invited to dinner tonight at my house, {guests[0].title()}.")
+print(f"You are invited to dinner tonight at my house, {guests[1].title()}.")
+print(f"You are invited to dinner tonight at my house, {guests[2].title()}.\n")
+
+
+# OR ehmatthes.github.io answer:
+
+guests = ['seha', 'eunji', 'miru']
+
+name = guests[0].title()
+print(f"I welcome you my dear {name} to my house for dinner.")
+
+name = guests[1].title()
+print(f"I welcome you my dear {name} to my house for dinner.")
+
+name = guests[2].title()
+print(f"I welcome you my dear {name} to my house for dinner.\n")
