6e126b0e990bbc1099d1fe0ff1d2dc3ad9f28d66
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-01-05 21:35:52 +0800

    Create 6-1. Person.py

---
+++ 6. dictionaries/6. exercises/6-1. Person.py
@@ -0,0 +1,14 @@
+''' Use a dictionary to store about a person you know. store their
+first name, last name, age, and the city in which they live. you should have keys such as first_name, last_name, age and city. print each piece of info stored in your dictionary. '''
+
+eunji = {
+	'first_name': 'eunji', 
+	'last_name': 'jeong', 
+	'age': 28, 
+	'city': 'busan'
+	}
+
+print(eunji['first_name'].title())
+print(eunji['last_name'].title())
+print(eunji['age'])
+print(eunji['city'].title())
