7d8504f806a7adc5bf470b9c35d21dab1c5462a6
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-01-05 21:10:24 +0800

    Create variables.py

---
+++ 2. variables and simple data types/2. exercises/variables.py
@@ -0,0 +1,10 @@
+#c2; pg19
+''' Variable '''
+
+# 2-1. simple message:
+simple_message = "Write a separate program to accomplish each of these exercises. Save each program with a filename that follows standard Python conventions, using lowercase letters and underscores, such as simple_message.py and simple_messages.py"
+print(simple_message)
+
+# 2-2. simple messages:
+simple_message = "\nHello to world of Python, everyone!"
+print(simple_message.title())
