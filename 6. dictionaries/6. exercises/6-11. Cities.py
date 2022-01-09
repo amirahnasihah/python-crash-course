a5d6b419272010b777324de571be1c12015f8199
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-01-10 04:05:56 +0800

    Update 6-11. Cities.py

--- 6. dictionaries/6. exercises/6-11. Cities.py
+++ 6. dictionaries/6. exercises/6-11. Cities.py
@@ -5,28 +5,36 @@ about that city. The keys for each city’s dictionary should be something like
 country, population, and fact. Print the name of each city and all of the information
 you have stored about it. '''
 
+print("My answer:")
 
 cities = {
     'kuala lumpur': {
         'country': 'malaysia',
         'population': 1_808_000,
-        'fact': 'famous with borneo rainforest',
+        'fact': 'famous with Borneo rainforest',
         },
 
     'paris': {
         'country': 'france',
         'population': 2_161_000,
-        'fact': 'eiffel tower',
+        'fact': 'famous with Eiffel Tower',
         },
 
     'london': {
         'country': 'united kingdom',
         'population': 8_982_000,
-        'fact': 'biggest city in Europe and' 
+        'fact': 'biggest city in Europe and ' 
         'largest city in the world'
         },
 
     }
 
 for city, city_info in cities.items():
-    print(f"City: {city}")
+    print(f"City: {city.title()}")
+    country = city_info['country']
+    population = f"{city_info['population']}"
+    fact = f"{city_info['fact']}"
+
+    print(f"\tCountry: {country.title()}")
+    print(f"\tPopulation: {population}")
+    print(f"\tFact: {fact[0].upper() + fact[1:]}")
