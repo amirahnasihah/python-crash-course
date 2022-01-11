e7edd6afd7a9c08de1d5c58903e611d8e8e9264e
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-01-11 23:55:11 +0800

    update even_or_odd.py

--- 7. user input and while loops/even_or_odd.py
+++ 7. user input and while loops/even_or_odd.py
@@ -1,12 +1,11 @@
 ''' How the input() function Works '''
 
-print("''' Using int() to Accept Numerical Input '''")
+print("''' The Modulo Operatod '''")
 
-height = input("How tall are you, in inches? ")
+number = input("How tall are you, in inches? ")
 height = int(height)
 
 if height >= 48:
   print("\nYou're tall enough to ride!")
 else:
   print("\nYou'll be able to ride when you're little older.")
-
