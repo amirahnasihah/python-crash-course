d22dd3f9cbf1dcb6e79f027ff6e9c9436c49f39d
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-01-15 19:54:02 +0800

    Delete 7-3. Multiple of Ten.py

--- 7. user input and while loops/7-3. Multiple of Ten.py
+++
@@ -1,9 +0,0 @@
-''' "Ask the user for a number, and then report whether the number is a multiple of 10 or not. '''
-
-number = input("Give me a number: ")
-number = int(number)
-
-if number % 10 == 0:
-    print(f"{number} is a multiple of 10.")
-else:
-    print(f"{number} is not a multiple of 10.")
