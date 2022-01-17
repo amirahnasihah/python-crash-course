ce95f78d6687471dccab5a8e1bcb2ea7147b482e
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-01-18 03:18:06 +0800

    Update rollercoaster.py

--- 7. user input and while loops/rollercoaster.py
+++ 7. user input and while loops/rollercoaster.py
@@ -1,5 +1,20 @@
 ''' How the input() function Works '''
 
+print("\n''' Using int() to Accept Numerical Input '''")
+
+age = input("How old are you? ")
+print(age)      # a string
+
+# use int() function to treat input as a numerical value
+
+age = input("How old are you? ")
+age = int(age)
+print(age)      # an integer
+
+# Python interpreted the input as a string because the number is 
+# enclosed in quotes. To be an integer it need to use int() function.
+
+
 print("''' Using int() to Accept Numerical Input '''")
 
 height = input("How tall are you, in inches? ")
@@ -9,3 +24,4 @@ if height >= 48:
   print("\nYou're tall enough to ride!")
 else:
   print("\nYou'll be able to ride when you're little older.")
+  
