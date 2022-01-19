2fcde8c128faf43dec514a36900bc9a82eba7c26
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-01-19 17:04:57 +0800

    update greeter.py

--- 7. user input and while loops/greeter.py
+++ 7. user input and while loops/greeter.py
@@ -10,7 +10,7 @@ name = input("Please enter your name: ")
 print(f"\nHelo, {name}!")
 
 # write a prompt longer than one line
-# prompt (or instructions) to display
+# prompt (or instructions) to display to user what to do.
 
 prompt = "If you tell us who you are, we can personalize the messages you see."
 prompt += "\nWhat is your first name? "
