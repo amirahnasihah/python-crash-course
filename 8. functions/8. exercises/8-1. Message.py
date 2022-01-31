bae416481d212c30f1167ffb2941fde35af72a0f
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-02-01 02:04:23 +0800

    Add files via upload

---
+++ 8. functions/8. exercises/8-1. Message.py
@@ -0,0 +1,6 @@
+def display_message():
+    """What to learn in this chapter."""
+    msg = "Learn how to write functions which responsible to perform a certain task I want."
+    print(msg)
+    
+display_message()
